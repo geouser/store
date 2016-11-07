845eb174fb613b14a7791fe129fc74bbfdb857c4
Serhiy Hulyi <serhiyhulyi93@gmail.com>
2016-11-07 23:25:19 +0200

    sdf

--- js/main.js
+++ js/main.js
@@ -238,60 +238,42 @@ jQuery(document).ready(function($) {
 
 
     /*----------------------------
-                              SEND FORM
+                              Google map
     -------------------------*/
-    /**
-     *
-     * Open popup
-     *
-     * @param popup {String} jQuery object (#popup)
-     *
-     * @return n/a
-     *
-    */
-    function openPopup(popup){
-        $.magnificPopup.open({
-            items: {
-              src: popup
-            },
-            type: 'inline',
-            fixedContentPos: false,
-            fixedBgPos: true,
-            overflowY: 'auto',
-            modal: false,
-            closeBtnInside: true,
-            preloader: false,
-            midClick: true,
-            removalDelay: 300,
-            mainClass: 'my-mfp-slide-bottom'
-        }, 0);
+    var map;
+    function googleMap_initialize() {
+        var lat = $('#map_canvas').data('lat');
+        var long = $('#map_canvas').data('lng');
+
+        var mapCenterCoord = new google.maps.LatLng(lat, long);
+        var mapMarkerCoord = new google.maps.LatLng(lat, long);
+
+
+        var mapOptions = {
+            center: mapCenterCoord,
+            zoom: 8,
+            //draggable: false,
+            disableDefaultUI: true,
+            scrollwheel: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+        var markerImage = new google.maps.MarkerImage('images/location.png');
+        var marker = new google.maps.Marker({
+            icon: markerImage,
+            position: mapMarkerCoord, 
+            map: map,
+            title:"Auro Architect"
+        });
+      
+        $(window).resize(function (){
+            map.setCenter(mapCenterCoord);
+        });
     }
 
-    $('form').on('submit', function(event) {
-        event.preventDefault();
-        /* Act on the event */
-        var data = $(this).serialize();
-        $.ajax({
-            url: theme.url + '/forms.php',
-            type: 'POST',
-            data: data,
-            success: function(result){
-                if (result.status == 'ok') {
-                    openPopup('#modal-popup-ok')
-                } else {
-                    openPopup('#modal-popup-error')
-                }
-            },
-            error: function(result) {
-                openPopup('#modal-popup-error');
-            }
-        })
-        .always(function() {
-            $('form').each(function(index, el) {
-                $(this)[0].reset();
-            });
-        });
-        
-    });
+    if ( $('#map_canvas').length > 0) {
+        googleMap_initialize();   
+    }
 
 }); // end file
